8cb6fc1c2169ef8832b25e888d7745088b6d784b
Darren Miller <darrmill@microsoft.com>
2021-09-24 13:28:50 -0700

    Make image reference specific in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,9 @@ module.exports = async (env, options) => {
         {
           test: /\.(png|jpg|jpeg|gif|ico)$/,
           type: "asset/resource",
+          generator: {
+            filename: "assets/[name][ext][query]",
+          },
         },
       ],
     },
